b8cdd0329cf2db3bf90034e4f6a324f11cf53912
Chris Jacobs <chris@chrisjacobs.com>
2013-05-06 16:33:09 -0700

    Updated Navigation for index, identity, industrial, playground and global.

--- global/header.php
+++ global/header.php
@@ -1,23 +1,23 @@
 <div class="container">
 <!--    <div class="scribble"></div>-->
 <nav class="navbar">
-	<div class="navbar-inner">
+	<div class="navbar-inner navbargradient">
     	<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-		</a>
-    <!--<a class="brand visible-phone visible-tablet hidden-desktop" href="#">Select</a>-->
+		</a>&nbsp;
+  
     
     <div class="nav-collapse">
-    <ul class="nav nav-pills">
+    <ul class="nav navhel">
       <li><a href="index.html">Home</a> </li>
       <li class="active"><a href="ipg.php">Interactive</a></li>
       <li><a href="identity.html">Identity</a></li>
       <li><a href="industrial.html">Industrial Design</a></li>
-      <li><a href="http://www.chrisjacobs.com/playground">Playground</a></li>
-      <li><a href="/chrisjacobs_portfolio.pdf">PDF Portfolio</a></li>
-     </ul>
+      <li><a href="playground/">Playground</a></li>
+      <li><a href="chrisjacobs_portfolio.pdf">PDF Portfolio</a></li>
+    </ul>
     </div>
 
     </div>
